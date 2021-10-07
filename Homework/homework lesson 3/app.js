8b2137376078b954faf59f938c036f9774b68aed
TQ_09Pro <phuochuunguyen2009@gmail.com>
2021-10-07 22:25:12 +0700

    Update app.js

--- Homework/homework lesson 3/app.js
+++ Homework/homework lesson 3/app.js
@@ -14,7 +14,48 @@ function task1(){
 //task 2 
 function task2(){
     var leap = prompt("Enter your year :","Example : 2000")
-    if (leap % 4 == 0  && leap % 100 == 0 || leap % 4 ==0 ) {
-        alert("the year" +" "+ leap +" "+ "is a leap year")
+    if (leap % 4 != 0) {
+        alert("the year" +" "+ leap +" "+ "is not a leap year")
+    }
+    else {
+        if (leap % 100 != 0 && leap % 4 == 0) {
+            alert("the year" +" "+ leap +" "+ "is a leap year")
+        }
+        else if (leap % 100 == 0) {
+            if (leap % 400 != 0) {
+                alert("the year" +" "+ leap +" "+ "is a not leap year")
+            }
+            else if (leap % 100 ==0) {
+                alert("the year" +" "+ leap +" "+ "is a leap year")
+            }
+        }
+    }
+}
+//task 3
+function task3(){
+    var a = prompt("Enter your number :")
+    var b = prompt("Enter your number :")
+    var c = prompt("Enter your number :")
+    if (a == 0 && b == 0 && c == 0) {
+        alert("there's nothing to be solved.")
+    }
+    if (a == 0 && b == 0 && c != 0) {
+        alert("the equation has no root.")
+    }
+    if (a == 0 && b != 0 && c != 0) {
+        x  = - c/b
+        alert(x)
+    }
+    if (a != 0 && b != 0 && c != 0){
+        var answer = b*b - 4*a*c
+        if (answer < 0) {
+            alert("the equation has no real root.")
+        }
+        else if (answer != 0){
+            x1 =  double.dsqrt = Math.sqrt( -b + (b * b - 4 * a * c) / (2*a));
+            x2 =  double.dsqrt = Math.sqrt (- b - (b * b - 4 * a * c) / (2*a));
+            alert("roots 1 is " + x1)
+            alert("roots 2 is " + x2)
+        }
     }
 }
