4647827d1864aff7677bbbb623123b0951ea4346
TQ_09Pro <phuochuunguyen2009@gmail.com>
2022-03-25 07:41:53 +0700

    GG

--- news_project/code_folder/Login/login.js
+++ news_project/code_folder/Login/login.js
@@ -2,12 +2,12 @@
 const loginbtn = document.getElementById("login-btn")
 loginbtn.addEventListener("click", function(e){
    let ripple = document.createElement("span")
-   this.appendChild(ripple)
-   let x = e.clientX - e.target.offsetLeft
-   let y = e.clientY - e.target.offsetTop
-   ripple.style.left = `${x}px`
-   ripple.style.top = `${y}px`
-   setTimeout(() =>{
+    this.appendChild(ripple)
+    let x = e.clientX - e.target.offsetLetf
+    let y = e.clientY - e.target.offsetTop
+    ripple.style.left = `${x}px`
+    ripple.style.Top = `${y}px`
+    setTimeout(() =>{
         ripple.remove()
    },1000)
 })
---
+++ news_project/code_folder/Sigh in/sighin.js
@@ -0,0 +1,13 @@
+//sighin-btn effect
+const sighinbtn = document.getElementById("sighin-btn")
+sighinbtn.addEventListener("click",function(e){
+    let ripple = document.createElement("span")
+    this.appendChild(ripple)
+    let x = e.clientX - e.target.offsetLetf
+    let y = e.clientY - e.target.offsetTop
+    ripple.style.left = `${x}px`
+    ripple.style.Top = `${y}px`
+    setTimeout(() =>{
+        ripple.remove()
+   },1000)
+})
