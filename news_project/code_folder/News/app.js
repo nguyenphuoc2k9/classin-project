59c65dc5cc19072487637cf2d50f54701246cc29
TQ_09Pro <phuochuunguyen2009@gmail.com>
2022-04-09 21:42:21 +0700

    gg

--- news_project/code_folder/News/app.js
+++ news_project/code_folder/News/app.js
@@ -1,5 +1,16 @@
 //sidenav
-const sidenav = document.getElementsByClassName("side-icon")
-sidenav[1].addEventListener("click", function(){
-    if()
+const sidenav = document.getElementById("sidenav")
+const menu = document.getElementById("menu")
+
+sidenav.style.right = "-300px";
+
+menu.addEventListener("click", function(){
+    if(sidenav.style.right == "-300px"){
+        sidenav.style.right ="0";
+        menu.src = "../close.png"
+    }
+    else{
+        sidenav.style.right = "-300px";
+        menu.src = "../menu.png"
+    }
 })
