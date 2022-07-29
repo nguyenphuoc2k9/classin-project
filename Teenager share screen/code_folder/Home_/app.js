111b6c322ee33edb85f45a852599eea9692cf135
TQ_09Pro <89725047+nguyenphuoc2k9@users.noreply.github.com>
2022-07-29 22:38:09 +0700

    gg

---
+++ Teenager share screen/code_folder/Home_/app.js
@@ -0,0 +1,17 @@
+var menubtn = document.getElementById("close")
+var btn = document.getElementById("close-")
+var sidenav = document.getElementById("sidenav-box")
+    sidenav.style.left = "-30%"
+    btn.style.left = "3%"
+menubtn.onclick=()=>{
+    if(sidenav.style.left == "0%"){
+        sidenav.style.left = "-30%"
+        menubtn.innerHTML = `<i class="fa-solid fa-bars"></i>`
+        btn.style.left = "3%"
+    } else {
+        sidenav.style.left = "0%"
+        menubtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+        btn.style.left = "21%"
+    }
+
+}
