09c557fe3f101d39dd387825cf9969f58b1e3c7c
TQ_09Pro <89725047+nguyenphuoc2k9@users.noreply.github.com>
2022-11-13 17:02:08 +0700

    gg

--- VFI/code_folder/Virus Forecast/Virus dictionary/home/app.js
+++ VFI/code_folder/Virus Forecast/Virus dictionary/home/app.js
@@ -3,84 +3,82 @@ const select_btn = select.querySelector(".select-btn")
 const options = select.querySelector(".options")
 const search_country = document.getElementById("search-country")
 var option = document.getElementsByClassName("option")
+let data;
+
 fetch("https://api.covid19api.com/summary")
-    .then((repose) => repose.json())
-    .then(function (data) {
-        if (data.Message != "Caching In progress") {
-            console.log(data);
-            printcountry(data)
-            document.getElementById("con-total").innerText = data.Global.TotalConfirmed
-            document.getElementById("con-today").innerText = data.Global.NewConfirmed
-            document.getElementById("dea-total").innerText = data.Global.TotalDeaths
-            document.getElementById("dea-today").innerText = data.Global.NewDeaths
-        }
-    })
-function printcountry(e) {
-    var country = e.Countries
-    var country_arr = []
-    console.log("lol".toLowerCase());
-    console.log(country);
+  .then((res) => {
+    if (!res.ok) {
+      showError();
+    } else {
+      res.json().then((info) => {
+        data = info;
 
-    for (let i = 0; i < country.length; i++) {
-        country_arr.push(country[i].Country)
-        let data = country[i - 1]
-        var html = ` <li class = 'option'value='${country[i].Country}'>${country[i].Country}</li>`
-        document.getElementsByClassName("options")[0].insertAdjacentHTML("beforeend", html)
-        option[i].addEventListener("click", () => {
-            handleClick(option[i], e, data, 'single')
-        })
-    }
-    search_country.addEventListener("keyup", function () {
-        let searchval = search_country.value
-        var list = country_arr.filter(data => {
-            String(data)
-            return data.includes(searchval)
-        })
-        options.innerHTML = list.map(data => `<li class = 'option' value='${data}'>${data}</li>`).join("") ? list.map(data => `<li class = 'option' value='${data}'>${data}</li>`).join("") : `<p>Oops! Country not found</p>`
-        handleClick(list, e, null, 'array')
-    })
-
-}
-function handleClick(dataList, e, datacountry, type) {
-        
-        console.log(type);
-        if (type == 'single' ) {
-            select_btn.firstElementChild.innerText = dataList.innerText
-            if (dataList.innerText == "World") {
-                document.getElementById("con-total").innerText = e.Global.TotalConfirmed
-                document.getElementById("con-today").innerText = e.Global.NewConfirmed
-                document.getElementById("dea-total").innerText = e.Global.TotalDeaths
-                document.getElementById("dea-today").innerText = e.Global.NewDeaths
-            } else {
-                document.getElementById("con-total").innerText = datacountry.TotalConfirmed
-                document.getElementById("con-today").innerText = datacountry.NewConfirmed
-                document.getElementById("dea-total").innerText = datacountry.TotalDeaths
-                document.getElementById("dea-today").innerText = datacountry.NewDeaths
-            }
+        if (info.Message === "Caching in progress") {
+          showError(info.Message);
         } else {
-            for (let i = 0; i < dataList.length; i++){
-            data = e.Countries[i - 1]
-            console.log(data);
-            option[i].addEventListener("click", function () {
-                if (option[i].innerText == "World") {
-                    document.getElementById("con-total").innerText = e.Global.TotalConfirmed
-                    document.getElementById("con-today").innerText = e.Global.NewConfirmed
-                    document.getElementById("dea-total").innerText = e.Global.TotalDeaths
-                    document.getElementById("dea-today").innerText = e.Global.NewDeaths
-                } else {
-                    document.getElementById("con-total").innerText = data.TotalConfirmed
-                    document.getElementById("con-today").innerText = data.NewConfirmed
-                    document.getElementById("dea-total").innerText = data.TotalDeaths
-                    document.getElementById("dea-today").innerText = data.NewDeaths
-                }
-                handleClick(dataList, e, data, 'array')
-            })
+          console.log(info);
+          document.querySelector(".countries").innerHTML = `
+            <button onclick="buttonClicked('World')">World</button>
+            ${info.Countries.map(
+              (country) =>
+                `<button onclick="buttonClicked('${country.Country}')">${country.Country}</button>`
+            ).join("")}
+          `;
+
+          showContent(info.Global);
         }
+      });
     }
+  })
+  .catch((err) => {
+    console.log(err);
+    showError();
+  });
 
-}
+document.querySelector("input").addEventListener("keyup", (e) => {
+  const value = e.target.value;
+  document.querySelectorAll(".countries button").forEach((item) => {
+    if (value.trim() === "") {
+      item.style.display = "block";
+    } else {
+      if (item.innerText.toLowerCase().includes(value.trim().toLowerCase())) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    }
+  });
+});
 
+const showError = (message = "") => {
+  document.querySelector(".loading").style.display = "none";
+  document.querySelector(".error").style.display = "flex";
+  document.querySelector(".main").style.display = "none";
+  if (message) {
+    document.querySelector(
+      ".error"
+    ).innerText = `Failed to load data. "${message}"`;
+  }
+};
 
+const buttonClicked = (id) => {
+  if (id === "World") {
+    showContent(data.Global);
+  } else {
+    showContent(data.Countries.find((country) => country.Country === id));
+  }
+};
+
+const showContent = (data) => {
+  document.querySelector(".loading").style.display = "none";
+  document.querySelector(".error").style.display = "none";
+  document.querySelector(".main").style.display = "block";
+
+  document.querySelector(".content").innerHTML = `
+    <p>New Confirmed: ${data.NewConfirmed}. New Deaths: ${data.NewDeaths}. New Recovered: ${data.NewRecovered}</p>
+    <p>Total Confirmed: ${data.TotalConfirmed}. Total Deaths: ${data.TotalDeaths}. Total Recovered: ${data.TotalRecovered}</p>
+  `;
+};
 //select
 
 select_btn.addEventListener("click", function () {
