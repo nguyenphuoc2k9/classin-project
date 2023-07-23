c6b17fa3f279cf57f4a74d559013a19ca3b0c8dc
TQ_09Pro <89725047+nguyenphuoc2k9@users.noreply.github.com>
2023-07-23 20:09:35 +0700

    ss

--- School_project/KHTT-2022-2023/TNTP/login_and_signup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns =[
-    path('login/',views.load_login_page),
-    path('more_info/',views.load_more_info),
-]
--- School_project/KHTT-2022-2023/TNTP/login_and_signup/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def load_login_page(re):
-    return render(re,'login.html')
-def load_more_info(re):
-    return render(re,'more-info.html')
